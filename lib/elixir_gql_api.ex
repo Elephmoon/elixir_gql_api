51d34ce40da003798ba038b72db9ac0edaff5c66
Alexandr Kizilow <alexandr.kizilov@pm.me>
2022-12-13 22:16:10 +0300

    mix initial (#1)
    Co-authored-by: Kizilov Aleksandr Nikolaevich

---
+++ lib/elixir_gql_api.ex
@@ -0,0 +1,18 @@
+defmodule ElixirGqlApi do
+  @moduledoc """
+  Documentation for `ElixirGqlApi`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirGqlApi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
